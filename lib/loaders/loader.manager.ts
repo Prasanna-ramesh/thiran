a17a5e85b404a6cc9b8955720f078db222b3fbf0
Prasanna-ramesh <prasannaramesh1994@gmail.com>
2025-07-22 13:05:49 +0200

    feat: merge loaded configs

--- lib/loaders/loader.manager.ts
+++ lib/loaders/loader.manager.ts
@@ -1,8 +1,10 @@
 import { existsSync } from 'node:fs';
 import { join } from 'node:path';
 import { resolve } from 'app-root-path';
-import { registry } from '../helper/registry';
+import { DEFAULT_PROFILE_NAME } from '../constants/default-config';
+import { registry, type Store } from '../helper/registry';
 import type { Strategies, SupportedFiles } from '../types';
+import { camelizeAndMerge, get, isObject } from '../utils/object';
 
 export class LoaderManager {
 	private readonly supportedExtensions: Record<SupportedFiles, string[]> = {
@@ -10,33 +12,49 @@ export class LoaderManager {
 		json: ['.json'],
 	};
 
-	constructor(private readonly loader: Strategies['loaders']) {}
+	private readonly environmentVariables: Store['environmentVariables'];
+	private readonly configProperties: Store['configProperties'];
+	private readonly activeProfiles: string[];
+
+	constructor(private readonly loader: Strategies['loaders']) {
+		this.environmentVariables = registry.strictGet('environmentVariables');
+		this.configProperties = registry.strictGet('configProperties');
+
+		const { activeProfiles } = this.configProperties;
+		this.activeProfiles = (this.environmentVariables[activeProfiles.name] ?? activeProfiles.defaultValue)
+			?.split(',')
+			.map((value) => value.trim()) ?? [DEFAULT_PROFILE_NAME];
+	}
 
 	/**
 	 * Invokes loader strategy based on the file extension
 	 *
 	 * @internal
 	 */
-	loadConfigurations(): unknown[] {
+	loadConfigurations(): Record<string, unknown> {
 		const configurationFilesLocation = this.getConfigurationFilesLocation();
 
-		return configurationFilesLocation.map((configurationFileLocation) => {
-			const isYaml = this.supportedExtensions.yaml.some((extension) => configurationFileLocation.endsWith(extension));
+		return configurationFilesLocation
+			.flatMap((configurationFileLocation) => {
+				const isYaml = this.supportedExtensions.yaml.some((extension) => configurationFileLocation.endsWith(extension));
 
-			if (isYaml) {
-				return this.loader.yaml.loadConfiguration(configurationFileLocation);
-			}
+				if (isYaml) {
+					return this.loader.yaml.loadConfiguration(configurationFileLocation);
+				}
 
-			const isJson = this.supportedExtensions.json.some((extension) => configurationFileLocation.endsWith(extension));
+				const isJson = this.supportedExtensions.json.some((extension) => configurationFileLocation.endsWith(extension));
 
-			if (isJson) {
-				return this.loader.json.loadConfiguration(configurationFileLocation);
-			}
+				if (isJson) {
+					const config = this.loader.json.loadConfiguration(configurationFileLocation);
+					console.log(config);
+					return config;
+				}
 
-			throw new Error(
-				`Cannot load ${configurationFileLocation}. Only ${Object.values(this.supportedExtensions).join(',')} are supported.`
-			);
-		});
+				throw new Error(
+					`Cannot load ${configurationFileLocation}. Only ${Object.values(this.supportedExtensions).join(',')} are supported.`
+				);
+			})
+			.reduce(this.mergeConfig, {});
 	}
 
 	/**
@@ -44,7 +62,7 @@ export class LoaderManager {
 	 *
 	 *
 	 * @remark
-	 * Also check if the file exists. If the file is missing, an error is thrown
+	 * Also checks if the file exists. If the file is missing, an error is thrown
 	 *
 	 * @internal
 	 *
@@ -76,5 +94,24 @@ export class LoaderManager {
 
 			return pathFromRoot;
 		});
+	}
+
+	private mergeConfig(mergedConfig: Record<string, unknown>, config: unknown): Record<string, unknown> {
+		if (!isObject(config)) {
+			return mergedConfig;
+		}
+
+		const profileInConfig = get<string>(config, this.configProperties.onProfile.name);
+
+		const isDefaultProfileActive = profileInConfig === null && this.activeProfiles.includes(DEFAULT_PROFILE_NAME);
+		const isActiveProfile = profileInConfig && this.activeProfiles.includes(profileInConfig);
+
+		const shouldMerge = isDefaultProfileActive || isActiveProfile;
+		if (shouldMerge) {
+			return camelizeAndMerge(mergedConfig, config);
+		}
+
+		// At this point, the profile
+		return mergedConfig;
 	}
 }
---
+++ lib/utils/object.ts
@@ -0,0 +1,99 @@
+import { camelCase } from './formatter';
+
+/**
+ * Function that asserts if a given unknown type is an object using type guard
+ *
+ * @param object Actual object
+ *
+ * @internal
+ */
+export const isObject = (object: unknown): object is Record<string, unknown> => {
+	return Boolean(object) && !Array.isArray(object) && typeof object === 'object';
+};
+
+/**
+ * Get value of a key (or nested key) from an object.
+ * Equivalent to .get() from lodash
+ *
+ * @param object Actual object
+ * @param path key of the property. Incase of nested property, it's dot separated
+ *
+ * @internal
+ */
+export const get = <T>(object: unknown, path: string): T | null => {
+	if (!isObject(object)) {
+		return null;
+	}
+
+	const keys = path.split('.');
+	let currentObject: unknown = object;
+
+	for (const key of keys) {
+		if (currentObject && typeof currentObject === 'object' && key in currentObject) {
+			currentObject = (currentObject as Record<string, unknown>)[key];
+		} else {
+			return null;
+		}
+	}
+
+	return currentObject as T;
+};
+
+/**
+ * To camelize the key and merge the keys recursively
+ *
+ * @param target Target Object
+ * @param source Source Object
+ * @param segments
+ *
+ * @internal
+ */
+export const camelizeAndMerge = (
+	target: Record<string, unknown>,
+	source: unknown,
+	segments: string[] = []
+): Record<string, unknown> => {
+	if (!isObject(source)) {
+		throw new Error('[objectUtil.camelizeAndMerge] Invalid source object');
+	}
+
+	Object.entries(source).map(([key, value]) => {
+		if (typeof value === 'object') {
+			camelizeAndMerge(target, source, [...segments, camelCase(key)]);
+		} else {
+			set(target, segments, value);
+		}
+	});
+
+	return target;
+};
+
+/**
+ * Set object value for a given segments
+ *
+ * @param object Source object
+ * @param segments Nested segments in a given object
+ * @param value Value to overwrite at the last segment
+ *
+ * @internal
+ */
+const set = (object: Record<string, unknown>, segments: string[], value: unknown): Record<string, unknown> => {
+	if (!isObject(object)) {
+		throw new Error('[objectUtil.set] Invalid object');
+	}
+
+	let currentObject = object;
+
+	for (const segment of segments) {
+		if (segments.at(-1) === segment) {
+			currentObject[segment] = value;
+		} else {
+			if (!(segment in currentObject)) {
+				currentObject[segment] = {};
+			}
+			currentObject = currentObject[segment] as Record<string, unknown>;
+		}
+	}
+
+	return object;
+};

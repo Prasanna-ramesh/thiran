33b9b0ec311dd44306c1bd6617f8269a1744ddfe
Prasanna-ramesh <prasanna.ramesh@scalable.capital>
2025-10-03 11:00:07 +0200

    feat: express example

--- examples/express/server.ts
+++ examples/express/server.ts
@@ -2,9 +2,28 @@ import express from 'express';
 import { ConfigManager } from 'thiran';
 import { z } from 'zod';
 
-const app = express();
+const tenantConfig = z.object({
+	iss: z.string(),
+	jwks: z.url(),
+	cacheValidity: z.number(),
+});
+
+const messagingConfig = z.object({
+	queueUrl: z.url(),
+});
+
 const validation = z.object({
 	port: z.number(),
+	logLevels: z.array(z.string()),
+	auth: z.object({
+		serverUrl: z.url(),
+		tenantA: tenantConfig,
+		tenantB: tenantConfig,
+	}),
+	messaging: z.object({
+		tenantA: messagingConfig,
+		tenantB: messagingConfig,
+	}),
 	database: z.object({
 		type: z.string(),
 		host: z.string(),
@@ -13,9 +32,23 @@ const validation = z.object({
 	}),
 });
 
-const configManager = new ConfigManager({ validationSchema: validation });
+const app = express();
+const configManager = new ConfigManager({
+	validationSchema: validation,
+	hooks: {
+		beforeValidate(config) {
+			return config;
+		},
+	},
+});
 
 configManager.load().then((config) => {
-	console.log(config);
-	app.listen(3000);
+	
+	app.get("/configs", (_, res) => {
+		res.send(config)
+	})
+
+	app.listen(config.port, () => {
+		console.log(`Server running on port ${config.port} 🚀`);
+	});
 });

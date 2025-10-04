be91ccf0efbe165723ace074998021f5cc63b7f6
Prasanna-ramesh <prasanna.ramesh@scalable.capital>
2025-10-04 11:06:36 +0200

    improvement: pain points when setting up example

--- lib/config-manager.ts
+++ lib/config-manager.ts
@@ -71,9 +71,14 @@ export class ConfigManager<Config = unknown> {
 			return this._config;
 		}
 
-		const issues = result.issues.map(({ message }) => message);
+		const issues = result.issues.map(({ message, path  }) => {
+			const fullPath = path?.map((segment) =>  typeof segment === 'object' ? segment.key : segment).join('.') ?? 'Missing path in issues'
+			
+			return `Path: ${fullPath}. Message: ${message}`
 
-		throw new Error(`Validation failed. Reason: ${issues.join('; ')}`);
+		}).join('\n');
+
+		throw new Error(`Validation failed. Reason: \n ${issues}`);
 	}
 
 	private camelizeConfigProperties() {

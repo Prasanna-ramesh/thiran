af7f92d2200bd28bd3502007f5b9001e71b22087
Prasanna Ramesh <44441571+Prasanna-ramesh@users.noreply.github.com>
2025-10-12 11:22:26 +0200

    chore: express example (#27)
    * chore: express example start
    
    * feat: express example
    
    * fix: unpublish docs
    
    * fix: lint
    
    * chore: first example completion

---
+++ docs/guides/examples/index.md
@@ -0,0 +1,3 @@
+## Examples
+
+- [Express](https://github.com/Prasanna-ramesh/thiran/tree/main/examples/express)
---
+++ examples/express/server.ts
@@ -0,0 +1,53 @@
+import express from 'express';
+import { ConfigManager } from 'thiran';
+import { z } from 'zod';
+
+const tenantConfig = z.object({
+	iss: z.string(),
+	jwks: z.url(),
+	cacheValidity: z.number(),
+});
+
+const messagingConfig = z.object({
+	queueUrl: z.url(),
+});
+
+const validation = z.object({
+	port: z.number(),
+	logLevels: z.array(z.string()),
+	auth: z.object({
+		serverUrl: z.url(),
+		tenantA: tenantConfig,
+		tenantB: tenantConfig,
+	}),
+	messaging: z.object({
+		tenantA: messagingConfig,
+		tenantB: messagingConfig,
+	}),
+	database: z.object({
+		type: z.string(),
+		host: z.string(),
+		port: z.number(),
+		username: z.string(),
+	}),
+});
+
+const app = express();
+const configManager = new ConfigManager({
+	validationSchema: validation,
+	hooks: {
+		beforeValidate(config) {
+			return config;
+		},
+	},
+});
+
+configManager.load().then((config) => {
+	app.get('/configs', (_, res) => {
+		res.send(config);
+	});
+
+	app.listen(config.port, () => {
+		console.log(`Server running on port ${config.port} 🚀`);
+	});
+});
